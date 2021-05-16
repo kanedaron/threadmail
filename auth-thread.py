4fde746c47afcf36b8f6f0b63cdf28db0f75628d
Nicolas Bonamour <nicolas.bonamour@inaubi.fr>
2021-05-16 22:06:18 +0200

    Finished (draft)cryptosystem

--- auth-thread.py
+++ auth-thread.py
@@ -1,11 +1,16 @@
+# X25519 Key Exchange
 from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey, X25519PublicKey
 from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import hashes, hmac
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 import base64
 
+# AES Symetric Cipher
 import os
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
+# Padding
+from cryptography.hazmat.primitives import padding
 
 import socket
 from threading import Thread, Event
@@ -14,7 +19,7 @@ from threading import Thread, Event
 env_publickey = base64.standard_b64decode(os.environ['HIS_PUBLICKEY'].encode(encoding="utf-8", errors="strict"))
 loaded_public_key = X25519PublicKey.from_public_bytes(env_publickey)
 env_private_key = base64.standard_b64decode(os.environ['PRIVATEKEY'].encode(encoding="utf-8", errors="strict"))
-private_key = X25519PrivateKey.from_private_bytes(os.environ['PRIVATEKEY'])
+private_key = X25519PrivateKey.from_private_bytes(env_private_key)
 shared_key = private_key.exchange(loaded_public_key)
 # Perform key derivation.
 derived_key = HKDF(
@@ -22,6 +27,12 @@ derived_key = HKDF(
     length=32,
     salt=None,
     info=b'handshake data',
+).derive(shared_key)
+HMAC_key = HKDF(
+    algorithm=hashes.SHA256(),
+    length=32,
+    salt=None,
+    info=b'hmac authentication code',
 ).derive(shared_key)
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -57,7 +68,17 @@ while True:
     if mail == "exit":
         mysock.send(mail.encode())
         break
-    mysock.send(mail.encode())
-
+    iv = os.urandom(16)
+    cipher = Cipher(algorithms.AES(derived_key), modes.CBC(iv))
+    encryptor = cipher.encryptor()
+    padder = padding.PKCS7(128).padder()
+    padded_data = padder.update(mail.encode())
+    padded_data += padder.finalize()
+    ciphertext = encryptor.update(padded_data) + encryptor.finalize()
+    hashmac = hmac.HMAC(HMAC_key, hashes.SHA256())
+    hashmac.update(ciphertext)
+    signature = hashmac.finalize()
+    mysock.send(signature)
+    mysock.send(signature+ciphertext)
 
 mysock.close()
