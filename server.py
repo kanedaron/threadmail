d3cb8cac6d6f3812594c2e0032c17f07143b7e34
Nicolas Bonamour <nicolas.bonamour@inaubi.fr>
2021-05-17 00:24:51 +0200

    Added counter-part to encrypted send

--- server.py
+++ server.py
@@ -1,13 +1,44 @@
+# X25519 Key Exchange
+from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey, X25519PublicKey
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes, hmac
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+import base64
+
+# AES Symetric Cipher
+import os
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
+# Padding
+from cryptography.hazmat.primitives import padding
+
 import socket
 from threading import Thread, Event
-
-# HMAC
-from cryptography.hazmat.primitives import hashes, hmac
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # mysock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 mysock.bind((socket.gethostname(), 8500))
 mysock.listen(5)
+
+# Setting up the cryptography
+env_publickey = base64.standard_b64decode(os.environ['HIS_PUBLICKEY'].encode(encoding="utf-8", errors="strict"))
+loaded_public_key = X25519PublicKey.from_public_bytes(env_publickey)
+env_private_key = base64.standard_b64decode(os.environ['PRIVATEKEY'].encode(encoding="utf-8", errors="strict"))
+private_key = X25519PrivateKey.from_private_bytes(env_private_key)
+shared_key = private_key.exchange(loaded_public_key)
+# Perform key derivation.
+derived_key = HKDF(
+    algorithm=hashes.SHA256(),
+    length=32,
+    salt=None,
+    info=b'handshake data',
+).derive(shared_key)
+HMAC_key = HKDF(
+    algorithm=hashes.SHA256(),
+    length=32,
+    salt=None,
+    info=b'hmac authentication code',
+).derive(shared_key)
 
 while True:
     # accept connections from outside
@@ -27,11 +58,11 @@ while True:
             hashmac = clientsocket.recv(512)
 
 
-            h = hmac.HMAC(key, hashes.SHA256())
+            h = hmac.HMAC(HMAC_key, hashes.SHA256())
             h.update(iv+ciphertext)
             h.verify(hashmac)
 
-            cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
+            cipher = Cipher(algorithms.AES(derived_key), modes.CBC(iv))
             decryptor = cipher.decryptor()
             message = decryptor.update(ciphertext) + decryptor.finalize()
             if message.decode() == "exit":
